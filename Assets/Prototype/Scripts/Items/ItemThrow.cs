dc6fc8dc77322710caec292dabbbbedfd7c7e3e4
Maikx <mikeybee2000@gmail.com>
2021-01-28 11:37:31 +0100

    Added Item Throw Script

---
+++ Assets/Prototype/Scripts/Items/ItemThrow.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemThrow : MonoBehaviour
+{
+    public Transform theDest;
+    public float throwForce = 10;
+    public bool canHold = true;
+    private Transform mousePos;
+    public Camera RMCamera;
+
+    private void Update()
+    {       
+        RaycastHit hit;
+        Ray ray = RMCamera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+
+            Vector3 hitPosition = hit.point;
+            theDest.transform.LookAt(hitPosition);
+            // Do something with the object that was hit by the raycast.
+        }
+    }
+
+    void OnMouseDown()
+    {
+            var mousePos = Input.mousePosition;
+            mousePos.z = 10.0f;
+            var objectPos = RMCamera.ScreenToWorldPoint(mousePos);
+            GetComponent<BoxCollider>().enabled = false;
+            GetComponent<Rigidbody>().useGravity = false;
+            this.transform.position = theDest.position;
+            this.transform.localRotation = theDest.rotation;
+            this.transform.parent = GameObject.Find("Destination").transform;
+     }
+
+    void OnMouseUp()
+    {
+            this.transform.parent = null;
+            GetComponent<Rigidbody>().useGravity = true;
+            GetComponent<Rigidbody>().velocity = theDest.transform.forward * throwForce;
+            GetComponent<BoxCollider>().enabled = true;
+    }
+}
