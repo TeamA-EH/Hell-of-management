fe8b3edd7e27e11567d2a936d59360887f65b6a4
Grayson1509 <alessiomanna.job@gmail.com>
2021-01-28 15:59:59 +0100

    added dash action

--- Assets/Prototype/Player/Scripts/MCController.cs
+++ Assets/Prototype/Player/Scripts/MCController.cs
@@ -27,8 +27,8 @@ public class MCController : MonoBehaviour, IDragger
     [Tooltip("Indica quanto tempo bisogni attendere prima di poter utilizzare nuovamente il DASH")]
     [SerializeField] float dashDowntime = 3f;
     public KeyCode dashKey = KeyCode.Space;
-    bool dash = false;
-    bool dashEnable = true;
+    [SerializeField] bool dash = false;
+    [SerializeField] bool dashEnable = true;
 
     [Header("Object Holding Movement"), Space(20)]
     [Tooltip("Definisce la scalabilita del movimento del personaggio rispetto al peso dell'oggetto")]
@@ -61,28 +61,21 @@ public class MCController : MonoBehaviour, IDragger
 
     public void Move(Vector3 _direction)
     {
-        cc.Move(((_direction * (walkingSpeed - decelleration)) + gravityForce) * Time.deltaTime);
-    }
-    void Dash(Vector3 _direction)
-    {
-        Debug.Log("Dash");
+        _direction = new Vector3(_direction.x, 0, _direction.z);
 
-        Vector3 endpoint = gameObject.transform.position + (new Vector3(_direction.x, 0, _direction.z).normalized * dashDistance);
-        gameObject.transform.DOMove(endpoint, dashDuration)
-            .OnComplete(() =>
-            {
-                dash = false;
-                StartCoroutine(PlayDashCooldown());
-            });
-        dash = false;
+        //cc.Move(((_direction * (walkingSpeed - decelleration)) + gravityForce) * Time.deltaTime);
+        cc.Move((_direction  + gravityForce) * Time.deltaTime);
     }
-    IEnumerator PlayDashCooldown()
+    
+    IEnumerator Dash()
     {
         dashEnable = false;
-        Debug.Log("Dash Disabled!");
+        dash = true;
+        PlayerSpeed = dashDistance / dashDuration;
         yield return new WaitForSeconds(dashDowntime);
+        dash = false;
         dashEnable = true;
-        Debug.Log("Dash Enabled!");
+
     }
 
     /* DRAG METHODS */
@@ -185,11 +178,13 @@ public class MCController : MonoBehaviour, IDragger
         else if (Input.GetKey(KeyCode.A)) horizontal = -Camera.main.transform.right;
 
         CameraAngularDirection = Vector3.SignedAngle(Camera.main.transform.forward, horizontal + vertical, Vector3.up);
-        PlayerSpeed = ((horizontal * Input.GetAxis("Horizontal")) + (vertical * Input.GetAxis("Vertical"))).magnitude * (walkingSpeed - decelleration);
+        if(!dash) PlayerSpeed = ((horizontal * Input.GetAxis("Horizontal")) + (vertical * Input.GetAxis("Vertical"))).magnitude * (walkingSpeed - decelleration);
         inputDirection = (Camera.main.transform.forward * Input.GetAxis("Vertical") + Camera.main.transform.right * Input.GetAxis("Horizontal")) * PlayerSpeed;
 
         //animator.SetFloat("speed", PlayerSpeed);
         //animator.SetFloat("Camera Angle", CameraAngularDirection);
+
+        if (dashEnable && Input.GetKeyDown(dashKey)) StartCoroutine(Dash());
 
         Move(inputDirection);
 
