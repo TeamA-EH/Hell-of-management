5abce28c85a06ba4712ba3900a02021c55f00397
Grayson1509 <alessiomanna.job@gmail.com>
2021-01-30 18:51:27 +0100

    added guest iteraction

---
+++ Assets/Prototype/Scripts/UI/TargetLookAtGameObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public sealed class TargetLookAtGameObject : MonoBehaviour
+{
+    [Header("Settings"), Space(20)]
+    [Tooltip("Indica l'oggetto di gioco verso cui l'oggetto a cui e' attaccato questo script deve guardare")]
+    [SerializeField] GameObject cameraBoomObject;
+
+    #region UnityCallbacks
+
+    private void Update()
+    {
+        gameObject.transform.LookAt(cameraBoomObject.transform);
+    }
+
+    #endregion
+}
