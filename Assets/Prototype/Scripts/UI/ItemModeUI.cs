910f0fb04c919301de16b6e7dedc91e156f9eee6
Grayson1509 <alessiomanna.job@gmail.com>
2021-01-30 12:44:28 +0100

    added item mode UI

---
+++ Assets/Prototype/Scripts/UI/ItemModeUI.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemModeUI : MonoBehaviour
+{
+    [SerializeField] Text modeText;
+
+    private void Update()
+    {
+        modeText.text = ItemUsabilityManager.GetInstance.ActiveMode.ToString();
+    }
+}
