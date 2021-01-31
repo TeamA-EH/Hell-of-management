3173584ea976551f08df685b45cd9d636169bb0a
Alex Silvestri <alexsilvestri.16@gmail.com>
2021-01-31 17:26:52 +0100

    "isometric" camera

--- Assets/Prototype/Scripts/Exa/CameraFollow.cs
+++ Assets/Prototype/Scripts/Exa/CameraFollow.cs
@@ -1,18 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject target;
+    [SerializeField] private Vector3 offset;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        this.transform.position = target.transform.position + offset;
     }
 }
