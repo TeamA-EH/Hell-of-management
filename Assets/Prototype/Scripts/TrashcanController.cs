d2c06cebaeb4491557f31f1d6fa25e04621d8504
Grayson1509 <alessiomanna.job@gmail.com>
2021-02-05 22:09:19 +0100

    added trash-can interactions n' trash-bag creation

---
+++ Assets/Prototype/Scripts/TrashcanController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System;
+
+[RequireComponent(typeof(WasteDetector))]
+[RequireComponent(typeof(WasteCollector))]
+[RequireComponent(typeof(WasteElaborator))]
+public class TrashcanController : MonoBehaviour
+{
+    WasteDetector wasteDetector;
+    WasteCollector wasteCollector;
+    WasteElaborator wasteElaborator;
+
+    #region UnityCallbakcs
+    private void OnEnable()
+    {
+        /* GET COMPONENTS FROM PARENT OBJECT */
+        if (!wasteDetector) wasteDetector = GetComponent<WasteDetector>();
+        if (!wasteCollector) wasteCollector = GetComponent<WasteCollector>();
+        if (!wasteElaborator) wasteElaborator = GetComponent<WasteElaborator>();
+
+        /* SUBSCRIBE EVENTS */
+        wasteDetector.OnWasteDetected += OnWasteDetected;
+
+        wasteCollector.OnWasteAdded += OnWasteAdded;
+        wasteCollector.OnMaxCapacityAchieved += OnMaxCapacityReached;
+
+        wasteElaborator.OnTrashBagCreated += OnTrashBagCreated;
+    }
+    #endregion
+
+    protected void OnWasteDetected(GameObject waste)
+    {
+        Destroy(waste);
+        wasteCollector.AddWaste();
+    }
+    protected void OnWasteAdded()
+    {
+        
+    }
+    protected void OnMaxCapacityReached()
+    {
+        wasteElaborator.CreateTrashbag(gameObject.transform.forward, 3);
+    }
+    protected void OnTrashBagCreated(GameObject trashbag)
+    {
+
+    }
+}
---
+++ Assets/Prototype/Scripts/WasteDetector.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System;
+
+[RequireComponent(typeof(Collider))]
+public class WasteDetector : MonoBehaviour
+{
+
+    public event Action<GameObject> OnWasteDetected;
+
+    #region UnityCallbacks
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.GetComponent<WasteController>() && other.gameObject.GetComponent<ThrowableItemStates>().state == ThrowableItemStates.States.Floating)
+        {
+            OnWasteDetected?.Invoke(other.gameObject);
+        }
+    }
+    #endregion
+}
