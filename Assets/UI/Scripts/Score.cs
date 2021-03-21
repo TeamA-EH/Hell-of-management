bbc09e81303d2d3ee2ba10b019bd39395beb59a3
Maikx <mikeybee2000@gmail.com>
2021-03-21 18:56:57 +0100

    Added Score/Stars Script + DialogueBox

--- Assets/UI/Scripts/Score.cs
+++ Assets/UI/Scripts/Score.cs
@@ -1,18 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Slider slider;
+
+    private float targetProgress = 0;
+
+    public void Awake()
     {
-        
+        slider = gameObject.GetComponent<Slider>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown("space"))
+            IncrementProgress(0.10f);
+    }
+
+    public void IncrementProgress(float newProgress)
+    {
+        targetProgress = slider.value += newProgress;
     }
 }
