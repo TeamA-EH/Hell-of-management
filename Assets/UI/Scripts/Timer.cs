2f69fadff624908f7a8a5290ded9b49bfd6536ef
Maikx <mikeybee2000@gmail.com>
2021-03-21 18:18:14 +0100

    Added Assets + Implemented Timer/Tasks

---
+++ Assets/UI/Scripts/Timer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Image circle;
+    public float totaltime = 60;
+
+    void Update()
+    {
+        totaltime = totaltime - Time.deltaTime;
+        //maxvalue/totaltime timer*famerate
+        circle.fillAmount -= 1 / totaltime * Time.deltaTime;
+    }
+}
