7eaf0c42465499392c22bcf26e698337f6427955
Maikx <mikeybee2000@gmail.com>
2021-05-02 16:48:39 +0200

    Fix Scenes, Updated IngameMenu

---
+++ Assets/Managers/Scripts/LevelManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HOM
+{
+    public class LevelManager : MonoBehaviour
+    {
+        private void Update()
+        {
+            GUIHandler.ActivatesMenu("Main Menu");
+        }
+    }
+}
