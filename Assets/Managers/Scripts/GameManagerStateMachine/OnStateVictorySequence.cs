a5664195e026818a610621a813da015b03253713
Maikx <mikeybee2000@gmail.com>
2021-05-11 16:28:35 +0200

    Victory/Defeat Screen appears when game ends

---
+++ Assets/Managers/Scripts/GameManagerStateMachine/OnStateVictorySequence.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnStateVictorySequence : StateMachineBehaviour
+{
+    public GameObject victoryScreen;
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        victoryScreen = Resources.Load<GameObject>("TempWonScreen (Canvas)");
+        Instantiate(victoryScreen, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+}
